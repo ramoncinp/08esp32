9912f632b9d2fc8266a0b975e68f488729a3ad44
ochocho <famsona@hotmail.es>
2018-01-04 14:42:43 -0600

    nada01/nada01.ino

---
+++ nada01/nada01.ino
@@ -0,0 +1,23 @@
+
+#define LED 2
+#define RETARDO 200
+unsigned long referencia;
+
+void setup()
+{
+  pinMode (LED, OUTPUT);
+  digitalWrite (LED, HIGH);
+}
+
+void loop()
+{
+  if (millis() - referencia > RETARDO)
+  {
+    referencia=millis();
+    int ver=digitalRead(LED);
+    if (ver==1)
+      digitalWrite (LED, LOW);
+    else
+      digitalWrite (LED, HIGH);
+  }
+}
