6ba1c9b44de496941b337d6e2d20dab43a941eef
RAMON PARRA <aldairparrag@gmail.com>
2018-01-09 08:46:07 -0600

    Blink asimetrico

--- nada01/nada01.ino
+++ nada01/nada01.ino
@@ -1,7 +1,9 @@
-
 #define LED 2
-#define RETARDO 2000
+#define RETARDO 1000
+#define RETARDO2 200
+
 unsigned long referencia;
+unsigned long referencia2;
 
 void setup()
 {
@@ -13,11 +15,17 @@ void loop()
 {
   if (millis() - referencia > RETARDO)
   {
-    referencia=millis();
-    int ver=digitalRead(LED);
-    if (ver==1)
-      digitalWrite (LED, LOW);
-    else
+    referencia = millis();
+    int ver = digitalRead(LED);
+    if (ver == 0)
       digitalWrite (LED, HIGH);
   }
+
+  if (millis() - referencia2 > RETARDO2)
+  {
+    referencia2 = millis();
+    int ver = digitalRead(LED);
+    if (ver == 1)
+      digitalWrite (LED, LOW);
+  }  
 }
